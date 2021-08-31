d89d6dc9c0d4c2dc844ac846435879db40998cd7
nathanbent <61810169+nathanbent@users.noreply.github.com>
2021-08-31 01:49:51 -0400

    Organized

--- backup.sh
+++ backup.sh
@@ -6,7 +6,6 @@
 
 #sudo dd if=/dev/sdb of=/media/pi/ExtUSB/bbypidf803021.img bs=1M count=10000
 
-#sudo dd if=/dev/mmcblk0 of=/media/pi/ExtUSB/bbypidsmall-803021.img bs=1M count=65
-# 00
+#sudo dd if=/dev/mmcblk0 of=/media/pi/ExtUSB/bbypidsmall-803021.img bs=1M count=6500
 
 #sudo pishrink.sh -z bbypidf803021.img
