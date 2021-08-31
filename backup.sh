72966825178926b45a6280d90acc16961b356e2a
nathanbent <61810169+nathanbent@users.noreply.github.com>
2021-08-31 01:28:58 -0400

    Initial commit

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#wget https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh#
+#sudo chmod +x pishrink.sh
+#sudo mv pishrink.sh /usr/local/bin
+
+#lsblk
+
+#sudo dd if=/dev/sdb of=/media/pi/ExtUSB/bbypidf803021.img bs=1M count=10000
+
+#sudo dd if=/dev/mmcblk0 of=/media/pi/ExtUSB/bbypidsmall-803021.img bs=1M count=6000
+
+#sudo pishrink.sh -z bbypidf803021.img
